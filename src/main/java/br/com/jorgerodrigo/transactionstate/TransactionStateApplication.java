3b3743289796a67e56adcc35c12e97e15c629a55
Jorge-Rodrigo <jorgerodrigo.monteiro19@gmail.com>
2024-03-24 11:59:55 -0300

    initial config

---
+++ src/main/java/br/com/jorgerodrigo/transactionstate/TransactionStateApplication.java
@@ -0,0 +1,13 @@
+package br.com.jorgerodrigo.transactionstate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionStateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionStateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/jorgerodrigo/transactionstate/TransactionStateApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jorgerodrigo.transactionstate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionStateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
